5597c9f333192ef2f84e2b332930e9539bb62f47
ChrisQA-GB <cyiangou@qa.com>
2021-11-09 13:57:53 +0000

    First Edit to JS to include no's in Table (broke)

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 "use strict";
 
+//let firstCard = Math.floor(Math.random + 2);
+//let secondCard = Math.floor(Math.random + 2);
+let firstCard = document.getElementById("firstCard");
+let secondCard = document.getElementById("secondCard");
 
-let firstCard = Math.floor(Math.random + 2);
-let secondCard = Math.floor(Math.random + 2);
+let valueOfCard1 = Math.floor(Math.random +2);
+let valueOfCard2 = Math.floor(Math.random +2); 
 
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let busted = true;
+let sum = valueOfCard1 + valueOfCard2;
+let blackkJack = false;
+let bust = true;
 let message = "";
 
 function startBlackJack() {
+
+  firstCard.innerText= valueOfCard1.toString();
+  secondCard.innerText= valueOfCard2.toString();
+
   if (sum < 20) {
     message = "Do you want to draw or fold";
   } else if (sum === 21) {
     message = "BLACKJACK";
-    hasBlackJack = true;
+    BlackJack = true;
   } else {
     message = "BUST";
-    busted = false;
+    bust = false;
   }
   console.log(message);
 }
