0df70692dd2ceb3e47d47c0284dc593e0945bf20
ChrisQA-GB <cyiangou@qa.com>
2021-11-09 17:37:06 +0000

    Added Sum count in table

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 //let secondCard = Math.floor(Math.random + 2);
 let firstCard = document.getElementById("firstCard");
 let secondCard = document.getElementById("secondCard");
+let sumOfCards = document.getElementById("sumOfCards");
 
 let valueOfCard1 = Math.floor(Math.random() * 10) + 2;
 let valueOfCard2 = Math.floor(Math.random() * 10) + 2; 
@@ -23,12 +24,17 @@ function startBlackJack() {
 
   if (sum < 20) {
     message = "Do you want to draw or fold";
+    sumOfCards.innerHTML = sum;
   } else if (sum === 21) {
     message = "BLACKJACK";
     BlackJack = true;
+    sumOfCards.innerHTML = sum;
   } else {
     message = "BUST";
     bust = false;
+    sumOfCards.innerHTML = sum;
   }
   console.log(message);
 }
+
+
