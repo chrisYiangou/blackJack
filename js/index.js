414f67b50cff840eb860744a0df2132c7f9962a7
chrisYiangou <cyiangou@qa.com>
2021-11-25 12:39:50 +0000

    Quick Fix to ensure the fold works

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@
 let player = {
   //Will include a picture
   name: "Chris",
-  pokerChips: 150
-}
+  pokerChips: 150,
+};
 
 let robot = {
-  imgFile: '/pictures/cowboyPokerMan.png',
+  imgFile: "/pictures/cowboyPokerMan.png",
   name: "Hanson",
-  pokerChips: 150
-}
+  pokerChips: 150,
+};
 
 /*These three variables are allowing me access the document.
 In this way I am able to play around with the InnerHTML and edit it to my hearts content*/
@@ -23,9 +23,8 @@ let firstCard = document.getElementById("firstCard");
 let secondCard = document.getElementById("secondCard");
 let sumOfCards = document.getElementById("sumOfCards");
 let messageHTML = document.getElementById("message");
-let hiddenMessageEl = document.getElementById("hiddenMessageEl")
+let hiddenMessageEl = document.getElementById("hiddenMessageEl");
 let twentyOneEl = document.getElementById("twentyOne");
-
 
 //Objects
 let playerEl = document.getElementById("player-el");
@@ -37,7 +36,7 @@ let foldButton = document.getElementById("foldButton-el");
 let drawButton = document.getElementById("drawButton-el");
 let resetButton = document.getElementById("reset-el");
 
-//Additional booleans and a message is required to help the players know how they are doing 
+//Additional booleans and a message is required to help the players know how they are doing
 // in the game
 
 //console.log(valueOfCard1);
@@ -54,7 +53,6 @@ let hasfolded = false;
 console.log(blackJack);
 console.log(bust);
 
-
 /*This is the actual value of the cards - I am making it random so that
 when a player starts the game, they will have two random cards to start with
 I've created the */
@@ -64,99 +62,89 @@ function randomCardValue() {
     //User will be able to choose between 1/11 (will figure this out later)
     return 11;
   } else if (randomValue > 10) {
-     return 10;
+    return 10;
   } else {
     return randomValue;
   }
 }
 //I need to create an Array of Card Value and the store them like this
-//An additional function sum will then be created so that I am able to add everything together 
+//An additional function sum will then be created so that I am able to add everything together
 //in the array
 
-
 function sum() {
-  let sumArrayCount = playerCards.length 
+  let sumArrayCount = playerCards.length;
 
-  for (let i = 0; i < sumArrayCount; i ++) {
-    cardSum += playerCards[i]
+  for (let i = 0; i < sumArrayCount; i++) {
+    cardSum += playerCards[i];
   }
   return cardSum;
 }
 
 /* This is the initial button to start the game - I will need to add three more buttons to complete 
 the functionality of the website. As well as this - we will create a "bot", who the player will play against 
-which should be good*/ 
+which should be good*/
 function startBlackJack() {
-
   let valueOfCard1 = randomCardValue();
   let valueOfCard2 = randomCardValue();
 
-  
   firstCard.innerHTML = valueOfCard1;
   secondCard.innerHTML = valueOfCard2;
-  playerEl.innerHTML = "Player Name: " + player.name + " || Poker Chips: " + player.pokerChips;
-  robotEl.innerHTML = "Opponents Name: " + robot.name + " || Poker Chips  " + robot.pokerChips;
+  playerEl.innerHTML =
+    "Player Name: " + player.name + " || Poker Chips: " + player.pokerChips;
+  robotEl.innerHTML =
+    "Opponents Name: " + robot.name + " || Poker Chips  " + robot.pokerChips;
 
   playerCards.push(valueOfCard1, valueOfCard2);
 
   renderBlackJack();
 
-  onClick="this.disabled=true;";
-  
-  
+  onClick = "this.disabled=true;";
 }
 
-//The startgame button should have a button that renders the game, as this needs to be used in the Draw button as well 
+//The startgame button should have a button that renders the game, as this needs to be used in the Draw button as well
 
 function renderBlackJack() {
-
   if (blackJack === false && bust === false && folded === false) {
+    sum();
 
-  sum();
-
-  if (cardSum <= 20) {
-    message = "Do you want to draw or fold";
-    sumOfCards.innerHTML = cardSum;
-  } else if (cardSum === 21) {
-    message = "BLACKJACK";
-    blackJack = true;
-    sumOfCards.innerHTML = cardSum;
-  } else {
-    message = "BUST";
-    bust = true;
-    sumOfCards.innerHTML = cardSum;
+    if (cardSum <= 20) {
+      message = "Do you want to draw or fold";
+      sumOfCards.innerHTML = cardSum;
+    } else if (cardSum === 21) {
+      message = "BLACKJACK";
+      blackJack = true;
+      sumOfCards.innerHTML = cardSum;
+    } else {
+      message = "BUST";
+      bust = true;
+      sumOfCards.innerHTML = cardSum;
+    }
+    messageHTML.innerHTML = message;
   }
-  messageHTML.innerHTML = message;
-
+  console.log(blackJack);
+  console.log(bust);
 }
-console.log(blackJack);
-console.log(bust)
-
-}
-
 
 /* Creating a function that is able to end the game will be my next step this willbe the FOLD button
  this will essentially just 
 change the game so that the message says Win Fail etc */
 
 function fold() {
+  if (blackJack === false && bust === false) {
+    folded = true;
 
-folded = true;
-
-  if (cardSum <= 20) {
-    hiddenMessage = "Could do better try again";
+    if (cardSum <= 20) {
+      hiddenMessage = "Could do better try again";
+    }
+    if (blackJack) {
+      hiddenMessage = "Well done on Winning this hand";
+    }
+    if (bust) {
+      hiddenMessage = "Try harder - Gamble More";
+    }
+    hiddenMessageEl.innerHTML = hiddenMessage;
   }
-  if (blackJack) {
-    hiddenMessage = "Well done on Winning this hand"
-  }
-  if (bust) {
-    hiddenMessage = "Try harder - Gamble More"
-  }
-  hiddenMessageEl.innerHTML = hiddenMessage;
-
 }
-
-
 
 /* The Draw Button will enable a user to be able to add a third, forth, fifth card and enable the user to keep 
 playing the game. */
@@ -165,16 +153,12 @@ function draw() {
   let newCard = randomCardValue();
   playerCards.push(newCard);
   renderBlackJack();
-  
 }
-
 
 //Creating a function to add to the already existing table
 function newCardtoTable() {
   let newCardRow = table.insertRow(-1);
 }
-
-
 
 /*This function is made to reset the webpage so that the values within the boxes are reset - In case the user wants to start again
 I should start thinking about how users can actually start saving their previous wins/losses...... */
@@ -182,13 +166,11 @@ function resetBlackJack() {
   window.location.reload();
 }
 
-//Replaced the onclicks with these eventlisteners  
+//Replaced the onclicks with these eventlisteners
 //Update EventHandlers were not working so have updated removed them for now
-//Uncomment below segment if needed in future  
+//Uncomment below segment if needed in future
 
 //startButton.addEventListener('click', startBlackJack());
 //foldButton.addEventListener('click', fold());
 //drawButton.addEventListener('click', draw());
 //resetButton.addEventListener('click', resetBlackJack());
-
-
